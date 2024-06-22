0f7c9f400ef9f471103f8f06d978da5cdc76fdcc
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-06-22 11:06:31 +0300

    Добавьте файлы проекта.

---
+++ SpDynamicLanguageRuntimeApp/Examples.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpDynamicLanguageRuntimeApp
+{
+    public class Examples
+    {
+        public static void DynamicExample()
+        {
+            object obj = 100;
+            int result = (int)obj + 200;
+            // obj += 200; // non compile
+
+
+            //dyn = "Hello";
+            int number = 100;
+            var var1 = number;
+            var1 += 200;
+
+
+            dynamic dyn = 100;
+            dyn += 100;
+            dyn = "Hello";
+            Console.WriteLine(dyn.Length);
+
+            //obj = "Hello";
+
+            //var var1 = 100;
+            //var var2 = "Hello"; 
+
+            Person person = new Person();
+            person.SetId(100);
+            person.SetId("ASD");
+
+        }
+
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        dynamic id;
+
+        public void SetId(dynamic id)
+        {
+            this.id = id;
+        }
+
+        public dynamic GetId()
+        {
+            return this.id;
+        }
+
+    }
+}
---
+++ SpDynamicLanguageRuntimeApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Dynamic;
+using System.Threading.Channels;
+
+dynamic Person = new ExpandoObject();
+
+Person.Name = "Bobby";
+Person.Age = 25;
+Person.Langs = new List<string>() { "C++", "JavaScript", "C#" };
+
+Person.Print = (Action)(() => Console.WriteLine($"Name: {Person.Name} Age: {Person.Age}"));
+Person.Print();
+
+foreach(var l in  Person.Langs)
+    Console.WriteLine(l);
+
+
+
+dynamic bob = new Employee();
+bob.Name = "Bobby";
+bob.Age = 25;
+bob.Salary = 50000m;
+
+Func<decimal, decimal> addSalary = (decimal amount) =>
+{
+    bob.Salary += amount;
+    return bob.Salary;
+};
+
+bob.AddSalary = addSalary;
+bob.AddSalary(30000m);
+Console.WriteLine(bob.Salary);
+
+
+class Employee : DynamicObject
+{
+    Dictionary<string, object> members = new Dictionary<string, object>();
+
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        if(value is not null)
+        {
+            members[binder.Name] = value;
+            return true;
+        }
+
+        return false;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+        result = null;
+        if(members.ContainsKey(binder.Name))
+        {
+            result = members[binder.Name];
+            return true;
+        }
+
+        return false;
+    }
+
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+    {
+        result = null;
+        if(members.ContainsKey(binder.Name))
+        {
+            if(args.Length == 1 && args?[0] is decimal amount)
+            {
+                dynamic method = members[binder.Name];
+                result = method.Invoke(amount);
+            }
+        }
+
+        return result != null;
+    }
+}
